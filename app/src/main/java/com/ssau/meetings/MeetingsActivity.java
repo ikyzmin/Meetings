0aa9917db1fbe1c47ccf77657f0aede7b1a57c1b
Kuzmin Ilya <i.kyzmin.v@gmail.com>
2016-12-03 10:54:32 +0400

    Initial commit

---
+++ app/src/main/java/com/ssau/meetings/MeetingsActivity.java
@@ -0,0 +1,26 @@
+package com.ssau.meetings;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MeetingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+}
