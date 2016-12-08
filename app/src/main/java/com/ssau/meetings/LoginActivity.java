f276b0e451178a78e4f70ce63a726db27d5c0f8b
Kuzmin Ilya <i.kyzmin.v@gmail.com>
2016-12-08 17:49:19 +0400

    implemented login screen

---
+++ app/src/main/java/com/ssau/meetings/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.ssau.meetings;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_login);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
